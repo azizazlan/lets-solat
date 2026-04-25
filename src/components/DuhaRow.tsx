dcc02db868ecbee7db574ee12e93964ec1b42224
dell <azizazlan@gmail.com>
2026-04-25 23:55:14 +0800

    To fix poster interval that display across iqamah period

--- src/components/DuhaRow.tsx
+++ src/components/DuhaRow.tsx
@@ -5,11 +5,13 @@ export default function DuhaRow(props: { dateDuha: Date; dateSyuruk?: Date }) {
     <div class="w-full text-black bg-white px-7 pb-5 flex flex-row items-center justify-between">
       {/* Left column: Duha */}
       <div class="flex flex-col items-center justify-start leading-relaxed">
-        <div class="text-7xl text-green-800 text-left" dir="rtl">
+        <div class="text-7xl text-green-800 text-left font-semibold" dir="rtl">
           يبدأ الضحى الساعة
         </div>
 
-        <div class="text-7xl text-green-800 mt-7">DUHA BERMULA</div>
+        <div class="text-7xl text-green-800 mt-7 font-semibold">
+          DUHA BERMULA
+        </div>
 
         <div class="text-9xl font-medium text-green-800">
           {padZero(props.dateDuha.getHours())}:
@@ -22,7 +24,7 @@ export default function DuhaRow(props: { dateDuha: Date; dateSyuruk?: Date }) {
         <div class="font-black text-7xl text-green-800" dir="rtl">
           الشروق
         </div>
-        <div class="text-7xl text-green-800 text-right mt-7">
+        <div class="font-semibold text-7xl text-green-800 text-right mt-7">
           MATAHARI TERBIT
         </div>
 
