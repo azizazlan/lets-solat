2b47a1a6888381cceff0442e9dec5d220d9c8def
dell <azizazlan@gmail.com>
2026-04-27 23:42:12 +0800

    Adjust spacing in the horizontal prayer times

--- src/components/PrayerHorizList.tsx
+++ src/components/PrayerHorizList.tsx
@@ -1,25 +1,5 @@
 import { For, createMemo } from "solid-js";
 import type { Prayer } from "@/types/prayers";
-import type { IqamahSettings } from "@/types/settings";
-import { getIqamahDurationInMins } from "@/services/settings";
-
-type IqamahKey = keyof IqamahSettings;
-
-function toIqamahKey(en: string): IqamahKey | null {
-  const key = en.toLowerCase();
-
-  if (
-    key === "alfajr" ||
-    key === "dhuhr" ||
-    key === "alasr" ||
-    key === "maghrib" ||
-    key === "alisha"
-  ) {
-    return key;
-  }
-
-  return null;
-}
 
 type Props = {
   filteredPrayers?: () => Prayer[];
@@ -40,7 +20,7 @@ export default function PrayerHorizList(props: Props) {
   const isActive = (p: Prayer) => activePrayer()?.en === p.en;
 
   return (
-    <div class="flex flex-row justify-between w-full pt-12 p-5">
+    <div class="flex flex-row justify-between w-full pt-12 p-12">
       {/* Prayer list */}
       <For each={props.filteredPrayers?.() || []}>
         {(p) => {
@@ -48,34 +28,22 @@ export default function PrayerHorizList(props: Props) {
 
           return (
             <div class="text-center">
-              {/* Arabic + English */}
               <div>
                 <div
-                  class={`mb-5 font-[Cairo] text-white ${active() ? "font-bold opacity-100 text-7xl" : "font-normal opacity-50 text-5xl"}`}
+                  class={`mb-5 font-[Cairo] text-white ${active() ? "font-bold opacity-100 text-7xl" : "font-semibold opacity-60 text-6xl"}`}
                 >
                   {p.ar}
                 </div>
-
                 <div
-                  class={`mb-5 text-white ${active() ? "font-bold opacity-100 text-6xl" : "font-normal opacity-50 text-5xl"}`}
+                  class={`mb-5 text-white ${active() ? "font-bold opacity-100 text-7xl" : "font-semibold opacity-60 text-6xl"}`}
                 >
                   {p.en}
                 </div>
               </div>
-
-              {/* Time + iqamah */}
               <div
-                class={`flex flex-col text-8xl text-white uppercase ${active() ? "font-bold opacity-100 animate-pulse" : "font-normal opacity-50"}`}
+                class={`flex flex-col text-white uppercase ${active() ? "text-8xl font-bold opacity-100" : "text-7xl font-semibold opacity-60"}`}
               >
                 <div>{p.time}</div>
-
-                <div class="text-xl opacity-70 normal-case">
-                  {(() => {
-                    const key = toIqamahKey(p.en);
-                    if (!key) return null;
-                    return `IQMH ${getIqamahDurationInMins(key)} mins`;
-                  })()}
-                </div>
               </div>
             </div>
           );
